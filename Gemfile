4ac5023bc1b48ca38a8056b3dad0a5cab0968163
Nicolas Grumberg <nicolas@vroomvroom.fr>
2022-11-16 15:40:09 +0100

    yelp MVP

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem "jbuilder"
 # Simple form
 gem "simple_form", github: "heartcombo/simple_form"
 
+# Faker
+gem "faker"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/migrate/20221116103813_create_restaurants.rb
+++ db/migrate/20221116103813_create_restaurants.rb
@@ -2,8 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :adress
-      t.integer :phone_number
+      t.string :address
+      t.string :phone_number
       t.string :category
 
       t.timestamps
